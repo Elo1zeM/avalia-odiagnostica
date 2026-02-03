c8f1bdff3aad1fbe66ee15f0f48a313e7da1ca20
Eloize <eloize.muzel@aluno.senai.br>
2026-02-03 13:47:46 -0300

    Add files via upload

---
+++ ex4.js
@@ -0,0 +1,10 @@
+let count = 5;
+console.log("Valor Inicial:", count);
+
+//Incrementar - Aumentar
+count++;
+console.log("Após incremento:", count);
+
+// Decrementar - Diminuir
+count--; 
+console.log("Após decremento:", count);
