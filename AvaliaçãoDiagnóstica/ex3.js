3e2858bb6aafd68e87d76f914326b93e71cf9665
Eloize <eloize.muzel@aluno.senai.br>
2026-02-03 14:10:13 -0300

    Delete AvaliaçãoDiagnóstica directory

--- AvaliaçãoDiagnóstica/ex3.js
+++
@@ -1,3 +0,0 @@
-let num = -15;
-let numPositivo = -num;
-console.log(numPositivo);
--- AvaliaçãoDiagnóstica/ex6.js
+++
@@ -1,7 +0,0 @@
-let p = true;
-let q = false;
-
-console.log(p && q);
-console.log(p || q);
-console.log(!p);
-console.log(!q);
